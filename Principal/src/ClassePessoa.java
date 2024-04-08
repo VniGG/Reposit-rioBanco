9d484772426dd7e66d0a5491761ec15665c8ca1c
lab04 <lab04@lab4-15>
2024-04-08 20:47:02 -0300

    as

--- Principal/src/ClassePessoa.java
+++ Principal/src/ClassePessoa.java
@@ -1,4 +1,6 @@
 
 public class ClassePessoa {
-
+	short atributoNumeroConta;
+	float aributoSaldoConta;
+	
 }
--- Principal/src/Classeprincipal.java
+++ Principal/src/Classeprincipal.java
@@ -20,6 +20,7 @@ public class Classeprincipal {
 		System.out.println(objeto_conta_dois.atributoNomeCliente);
 		System.out.println(objeto_conta_dois.atributoDocumentoCliente);
 		
+		//Adicionando dados das contas
 		objeto_conta_um.atributoNumeroConta = 123;
 		objeto_conta_um.aributoSaldoConta = 2_600.00F;
 		objeto_conta_um.atributoTipoConta = "Conta Pequena";
@@ -27,6 +28,21 @@ public class Classeprincipal {
 		objeto_conta_um.atributoNumeroConta = 789;
 		objeto_conta_um.aributoSaldoConta = 4_000.00F;
 		objeto_conta_um.atributoTipoConta = "Conta Corerente";
+		
+		System.out.println();
+		
+		System.out.println(objeto_conta_um.atributoNumeroConta);
+		System.out.println(objeto_conta_um.aributoSaldoConta);
+		System.out.println(objeto_conta_um.atributoTipoConta);
+		System.out.println(objeto_conta_um.atributoNomeCliente);
+		System.out.println(objeto_conta_um.atributoDocumentoCliente);
+		
+		System.out.println(objeto_conta_dois.atributoNumeroConta);
+		System.out.println(objeto_conta_dois.aributoSaldoConta);
+		System.out.println(objeto_conta_dois.atributoTipoConta);
+		System.out.println(objeto_conta_dois.atributoNomeCliente);
+		System.out.println(objeto_conta_dois.atributoDocumentoCliente);
+		
 		
 	}
 
