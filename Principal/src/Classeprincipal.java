31db6c4c0f3eed3af16d878317499a5d95e3bc4a
lab04 <lab04@lab4-15>
2024-04-08 20:18:07 -0300

    a

--- Principal/src/Classeprincipal.java
+++ Principal/src/Classeprincipal.java
@@ -20,6 +20,13 @@ public class Classeprincipal {
 		System.out.println(objeto_conta_dois.atributoNomeCliente);
 		System.out.println(objeto_conta_dois.atributoDocumentoCliente);
 		
+		objeto_conta_um.atributoNumeroConta = 123;
+		objeto_conta_um.aributoSaldoConta = 2_600.00F;
+		objeto_conta_um.atributoTipoConta = "Conta Pequena";
+		
+		objeto_conta_um.atributoNumeroConta = 789;
+		objeto_conta_um.aributoSaldoConta = 4_000.00F;
+		objeto_conta_um.atributoTipoConta = "Conta Corerente";
 		
 	}
 
